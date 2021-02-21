3325fbcd92af62038dd1d1b99f2a0e9c513cad1b
Daniel Pottenger <gamerpotty@gmail.com>
2021-02-21 22:55:02 +0000

    allow admin commands to ignore playername casing

--- src/server/Admin/AdminManager.server.lua
+++ src/server/Admin/AdminManager.server.lua
@@ -5,6 +5,16 @@ function IsAdmin(player)
     return adminSet.Contains(player);
 end
 
+local function GetPlayer(playerName)
+    for index, value in pairs(game.Players:GetChildren()) do
+        if(string.lower(value.Name) == string.lower(playerName)) then
+            return value;
+        end
+    end
+
+    return nil;
+end
+
 game.Players.PlayerAdded:connect(function(player)
     repeat wait() until adminSet.Initialized();
     if (IsAdmin(player)) then
@@ -12,12 +22,22 @@ game.Players.PlayerAdded:connect(function(player)
             local splitMessage = string.split(message, ' ');
             if(splitMessage[1] == '!tp') then
                 local to = splitMessage[2];
-                local toPlayer = game.Players[to];
+                local toPlayer = GetPlayer(to);
+
+                if(toPlayer == nil) then
+                    return;
+                end
+
                 local toPosition = toPlayer.Character:GetPrimaryPartCFrame().p;
                 player.Character:MoveTo(toPosition)
             elseif(splitMessage[1] == '!money') then
                 local to = splitMessage[2];
-                local toPlayer = game.Players[to];
+                local toPlayer = GetPlayer(to);
+
+                if(toPlayer == nil) then
+                    return;
+                end
+
                 local amount = splitMessage[3];
                 moneyManager.AddMoney(toPlayer, amount);
             end
