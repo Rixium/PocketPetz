7c250978a304a92c429f8520a433c8b12b95c0ef
Daniel Pottenger <gamerpotty@gmail.com>
2021-02-21 17:39:54 +0000

    allow press e key to continue dialog

--- src/client/PlayerInputScript.client.lua
+++ src/client/PlayerInputScript.client.lua
@@ -1,13 +1,8 @@
+local contextActionService = game:GetService("ContextActionService");
 local playerInteractor = require(game.Players.LocalPlayer.PlayerScripts.Client.PlayerInteractor);
 
-local dialogMenu = game.Players.LocalPlayer.PlayerGui:WaitForChild("Dialog GUI");
-local dialogScript = require(dialogMenu.Frame.DialogLabel.Dialog);
+function onInteractKeyPressed()
+	playerInteractor.Interact();
+end
 
-local player = game.Players.LocalPlayer;
-local platform = nil;
-
-repeat wait() until player.Character;
-local character = player.Character;
-local humanoid = character:WaitForChild("Humanoid");
-
-ContextActionService:BindAction("Interact", handleAction, true, Enum.KeyCode.E, Enum.KeyCode.ButtonR1);
+contextActionService:BindAction("Interact", onInteractKeyPressed, true, Enum.KeyCode.E, Enum.KeyCode.ButtonR1);
