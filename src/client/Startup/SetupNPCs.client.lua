7986aee7498553ff7cbb9089abc7288cff652715
Daniel Pottenger <gamerpotty@gmail.com>
2021-02-23 21:37:55 +0000

    setup local player profile GUI

--- src/client/Startup/SetupNPCs.client.lua
+++ src/client/Startup/SetupNPCs.client.lua
@@ -3,5 +3,21 @@ local npcCreator = require(game.Players.LocalPlayer.PlayerScripts.Client.Creator
 local npcPlacements = workspace.NPCs;
 
 for index, placement in pairs(npcPlacements:GetChildren()) do
-    npcCreator.New(placement);
+    spawn(function() 
+        npcCreator.New(placement);
+    end)
 end
+
+
+
+-- Profile
+local Players = game:GetService("Players")
+local mainGUI = game.Players.LocalPlayer.PlayerGui["Main GUI"];
+local profileGUI = mainGUI["Profile GUI"];
+
+profileGUI.HeaderFrame.NameLabel.Text = game.Players.LocalPlayer.Name;
+
+local userId = game.Players.LocalPlayer.UserId
+local thumbType = Enum.ThumbnailType.HeadShot
+local thumbSize = Enum.ThumbnailSize.Size420x420
+profileGUI.ProfileFrame.ProfilePicture.Image = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize);
