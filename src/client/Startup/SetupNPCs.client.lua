2e7755b37a360cbe26c8d1ba07bfe4ba05490f76
Daniel Pottenger <gamerpotty@gmail.com>
2021-02-23 23:14:36 +0000

    allow click on player to show profile

--- src/client/Startup/SetupNPCs.client.lua
+++ src/client/Startup/SetupNPCs.client.lua
@@ -3,11 +3,10 @@ local npcCreator = require(game.Players.LocalPlayer.PlayerScripts.Client.Creator
 local npcPlacements = workspace.NPCs;
 
 for index, placement in pairs(npcPlacements:GetChildren()) do
-    spawn(function() 
+     spawn(function() 
         npcCreator.New(placement);
     end)
 end
-
 
 
 -- Profile
@@ -21,3 +20,24 @@ local userId = game.Players.LocalPlayer.UserId
 local thumbType = Enum.ThumbnailType.HeadShot
 local thumbSize = Enum.ThumbnailSize.Size420x420
 profileGUI.ProfileFrame.ProfilePicture.Image = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize);
+
+-- Click Detection
+
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+
+local UserInputService = game:GetService("UserInputService")
+local camera = workspace.CurrentCamera;
+
+UserInputService.InputBegan:connect(function(input)
+	local pos = input.Position
+
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		local mouseLocation = pos;
+        local unscaled_ray = camera:ViewportPointToRay(mouseLocation.X,mouseLocation.Y)
+        local result = workspace:Raycast(unscaled_ray.Origin, unscaled_ray.Direction*1000)
+        if result.Instance.Parent:FindFirstChild("Humanoid") then
+            profileGUI.Visible = true;
+        end
+	end
+end)
