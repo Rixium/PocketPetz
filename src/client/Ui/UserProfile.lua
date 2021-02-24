1c402ef07cd8984a609e142e263b4fe6532e794e
Daniel Pottenger <gamerpotty@gmail.com>
2021-02-24 19:23:44 +0000

    fix buttons after ui update

--- src/client/Ui/UserProfile.lua
+++ src/client/Ui/UserProfile.lua
@@ -10,13 +10,16 @@ local profileGUI = mainGUI["Profile GUI"];
 -- Functions
 function UserProfile.Show(character)
     local profilePlayer = players:GetPlayerFromCharacter(character);
-    profileGUI.HeaderFrame.NameLabel.Text = profilePlayer.Name;
+    profileGUI.ProfileBack.HeaderFrame.NameLabel.Text = profilePlayer.Name;
 
     local userId = profilePlayer.UserId
     local thumbType = Enum.ThumbnailType.HeadShot
     local thumbSize = Enum.ThumbnailSize.Size420x420
 
-    profileGUI.ProfileFrame.ProfilePicture.Image = players:GetUserThumbnailAsync(userId, thumbType, thumbSize);
+    spawn(function () 
+        profileGUI.ProfileBack.ProfileFrame.ProfilePicture.Image = players:GetUserThumbnailAsync(userId, thumbType, thumbSize);
+    end)
+
     profileGUI.Visible = true;
 end
 
