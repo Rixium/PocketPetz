2331beec9c47fb82fec59f155c4e8c768709dac0
Daniel Pottenger <gamerpotty@gmail.com>
2021-03-02 10:05:58 +0000

    item pickup tweening

--- src/client/Startup/Events/ItemPickupEventSetup.client.lua
+++ src/client/Startup/Events/ItemPickupEventSetup.client.lua
@@ -20,19 +20,61 @@ local function ReceiveItemPickupEvent(itemPickupData)
         return;
     end
 
+    currentItemPickupData = itemPickupData;
+
     getItemPopup.Visible = true;
+
+    getItemPopup:TweenPosition(
+		UDim2.new(0.5, 0, 0.5, 0),
+		Enum.EasingDirection.Out,
+		Enum.EasingStyle.Sine,
+		0.5,
+		true
+	);
+
     getItemPopup.MessageFrame.MessageBack.Frame.NameLabel.Text = itemPickupData.Item.Name;
     getItemPopup.MessageFrame.MessageBack.Frame.Frame.MessageText.Text = itemPickupData.Body;
     getItemPopup.MessageFrame.MessageBack.FaceBack.FaceImage.Image = "rbxthumb://type=Asset&id=" .. itemPickupData.Item.ModelId .. "&w=150&h=150";
-    
-    getItemPopup.Frame.AcceptFrame.YesButton.MouseButton1Click:Connect(function()
-        itemApprovePickupEvent:FireServer(itemPickupData.Item.ItemId);
-        getItemPopup.Visible = false;
-    end)
-    getItemPopup.Frame.CancelFrame.NoButton.MouseButton1Click:Connect(function()
-        itemDeclinePickupEvent:FireServer(itemPickupData.Item.ItemId);
-        getItemPopup.Visible = false;
-    end)
 end
+
+getItemPopup.Frame.AcceptFrame.YesButton.MouseButton1Click:Connect(function()
+    if(currentItemPickupData == nil) then
+        return;
+    end
+
+    getItemPopup:TweenPosition(
+        UDim2.new(0.5, 0, 1.5, 0),
+        Enum.EasingDirection.In,
+        Enum.EasingStyle.Sine,
+        0.5,
+        true,
+        function()
+            getItemPopup.Visible = false;
+        end
+    );
+    itemApprovePickupEvent:FireServer(currentItemPickupData.Item.ItemId);
+    
+    currentItemPickupData = nil;
+end)
+
+getItemPopup.Frame.CancelFrame.NoButton.MouseButton1Click:Connect(function()
+    if(currentItemPickupData == nil) then
+        return;
+    end
+    
+    getItemPopup:TweenPosition(
+        UDim2.new(0.5, 0, 1.5, 0),
+        Enum.EasingDirection.In,
+        Enum.EasingStyle.Sine,
+        0.5,
+        true,
+        function()
+            getItemPopup.Visible = false;
+        end
+    );
+    itemDeclinePickupEvent:FireServer(currentItemPickupData.Item.ItemId);
+
+    currentItemPickupData = nil;
+end)
 
 itemPickupEvent.OnClientEvent:Connect(ReceiveItemPickupEvent);
