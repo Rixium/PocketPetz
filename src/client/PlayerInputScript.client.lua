9524220be41dd879a07860e0e381b7974e506d34
Rixium <gamerpotty@gmail.com>
2021-03-04 10:38:39 +0000

    allow click other stuff

--- src/client/PlayerInputScript.client.lua
+++ src/client/PlayerInputScript.client.lua
@@ -24,3 +24,30 @@ titleUnlocked.OnClientEvent:Connect(titlesMenu.SetupTitles);
 local titlesButton = mainGUI.Buttons.TitleButton.TitleButton.MouseButton1Click:Connect(function() 
     titlesMenu.Toggle();
 end)
+
+
+
+local userInputService = game:GetService("UserInputService");
+
+-- Functions
+function DoInput(pos)
+    local camera = workspace.CurrentCamera;
+    local unitRay = camera:ScreenPointToRay(pos.X, pos.Y);
+    local ray = Ray.new(unitRay.Origin, unitRay.Direction * 1000)
+
+    local result = game.Workspace:FindPartOnRay(ray);
+    if(result == nil or result.Parent == nil) then
+        return;
+    end
+
+    local ancestor = result:FindFirstAncestorOfClass("Part");
+    
+end
+ 
+userInputService.InputEnded:Connect(function(input, gameProcessed)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		DoInput(input.Position);
+	elseif input.UserInputType == Enum.UserInputType.Touch then
+		DoInput(input.Position);
+    end
+end);
