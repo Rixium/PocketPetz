979295f3321507ee37633a1f7cc569670cb85bcf
Rixium <gamerpotty@gmail.com>
2021-03-04 09:57:18 +0000

    add a level up function

--- src/server/Services/PetService.lua
+++ src/server/Services/PetService.lua
@@ -5,6 +5,11 @@ local dataStoreGet = require(serverScriptService.Server.DataStoreGet);
 local dataStore2 = dataStoreGet.DataStore;
 local itemsStore = "Items";
 
+local function LevelUpPet(player, pet) 
+    local remaining = pet.Data.CurrentExperience - pet.ExperienceToLevel;
+    pet.Data.CurrentExperience = remaining;
+end
+
 function PetService.AddExperience(player, guid, experienceAmount)
     local itemStore = dataStore2(itemsStore, player);
     local items = itemStore:Get({});
@@ -12,6 +17,11 @@ function PetService.AddExperience(player, guid, experienceAmount)
     for _, item in pairs(items) do
         if(item.Id == guid) then
             item.Data.CurrentExperience = item.Data.CurrentExperience + experienceAmount;
+
+            if(item.Data.CurrentExperience >= item.ExperienceToLevel) then
+                LevelUpPet(player, item);
+            end
+
             break;
         end
     end
