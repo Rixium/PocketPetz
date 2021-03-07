251bdc6101a1bae0fc7b661e04bde69b7e15fb4a
Daniel Pottenger <gamerpotty@gmail.com>
2021-03-07 20:54:02 +0000

    user menu tween and sounds

--- src/client/Ui/UserMenu.lua
+++ src/client/Ui/UserMenu.lua
@@ -4,6 +4,8 @@ local UserMenu = {};
 
 local players = game:GetService("Players");
 local userProfile = require(players.LocalPlayer.PlayerScripts.Client.Ui.UserProfile);
+local tweenService = game:GetService("TweenService");
+local replicatedStorage = game:GetService("ReplicatedStorage");
 
 -- Variables
 
@@ -11,15 +13,41 @@ local clickedPlayer = nil;
 
 -- Functions
 
-function UserMenu.Show(player, adornee)
+function UserMenu.Show(character, adornee)
+    local player = players:GetPlayerFromCharacter(character);
+
+    if(player.UserId == players.LocalPlayer.UserId) then
+        return
+    end
+
     clickedPlayer = player;
-    players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu.Enabled = true;
-    players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu.Adornee = adornee.HumanoidRootPart;
+
+    local menu = players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu;
+    menu.Adornee = adornee.HumanoidRootPart;
+    menu.Enabled = true;
+
+    replicatedStorage.ClickSound:Play();
+
+    local tweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Bounce, Enum.EasingDirection.Out)
+    local tween = tweenService:Create(menu.ProfileButton, tweenInfo, {Size=UDim2.new(0.4, 0,0.2, 0)})
+    local tween2 = tweenService:Create(menu.TradeButton, tweenInfo, {Size=UDim2.new(0.4, 0,0.2, 0)})
+    tween:Play()
+    tween2:Play()
 end
 
 function UserMenu.Hide()
-    players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu.Enabled = false;
-    players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu.Adornee = nil;
+    
+    local menu = players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu;
+
+    local tweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+    local tween = tweenService:Create(menu.ProfileButton, tweenInfo, {Size=UDim2.new(0,0,0,0)})
+    local tween2 = tweenService:Create(menu.TradeButton, tweenInfo, {Size=UDim2.new(0,0,0,0)})
+    tween:Play()
+    tween2:Play()
+    tween.Completed:Wait();
+
+    menu.Enabled = false;
+    menu.Adornee = nil;
 end
 
 players.LocalPlayer.PlayerGui:WaitForChild("Main GUI").UserMenu.ProfileButton.MouseButton1Click:Connect(function ()
