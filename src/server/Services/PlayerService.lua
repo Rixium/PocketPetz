0286f5cbc11cf56d4181f4296d6cb00607825bc9
Rixium <gamerpotty@gmail.com>
2021-03-15 17:44:05 +0000

    add legend checks to player service

--- src/server/Services/PlayerService.lua
+++ src/server/Services/PlayerService.lua
@@ -13,6 +13,9 @@ local dataStoreGet = require(serverScriptService.Server.DataStoreGet);
 local dataStore2 = dataStoreGet.DataStore;
 
 -- Variables
+local NOT_LEGEND = -1;
+local PERMENANT_LEGEND = -2;
+
 local playerInfoData = "PlayerInfo";
 
 -- Functions
@@ -25,7 +28,9 @@ function PlayerService.CreatePlayerInfo(player)
     end
 
     playerInfoStore:Set({
-        CurrentZone = "The Spawn"
+        CurrentZone = "The Spawn",
+        IsLegend = false,
+        LegendExpiration = NOT_LEGEND
     });
 
     return playerInfo;
@@ -34,7 +39,9 @@ end
 function PlayerService.GetPlayerInfo(player)
     local playerInfoStore = dataStore2(playerInfoData, player);
     local playerInfo = playerInfoStore:Get({ 
-        CurrentZone = "The Spawn"
+        CurrentZone = "The Spawn",
+        IsLegend = false,
+        LegendExpiration = NOT_LEGEND
     });
 
     playerInfoStore:Set(playerInfo);
@@ -52,6 +59,53 @@ function PlayerService.SetPlayerLocation(player, locationName)
     playerInfoStore:Set(playerInfo);
 
     playerSwitchedZone:FireClient(player, locationName);
+end
+
+function PlayerService.IsPlayerLegend(player) 
+    local playerInfoStore = dataStore2(playerInfoData, player);
+    local playerInfo = playerInfoStore:Get();
+
+    if(playerInfo == nil) then return false end
+
+    if(playerInfo.IsLegend == nil) then
+        playerInfo.IsLegend = false;
+        playerInfo.LegendExpiration = NOT_LEGEND;
+    end
+    
+    if(playerInfo.LegendExpiration == PERMENANT_LEGEND) then
+        playerInfo.IsLegend = true;
+    elseif(os.time() >= playerInfo.LegendExpiration) then
+        playerInfo.IsLegend = false;
+        playerInfo.LegendExpiration = NOT_LEGEND;
+    end
+
+    if(playerInfo.LegendExpiration == NOT_LEGEND) then
+        playerInfo.IsLegend = false;
+    end
+
+    playerInfoStore:Set(playerInfo);
+
+    return playerInfo.IsLegend;
+end
+
+function PlayerService.MakeLegendForDays(player, days)
+    local playerInfoStore = dataStore2(playerInfoData, player);
+    local playerInfo = playerInfoStore:Get();
+
+    playerInfo.LegendExpiration = os.time() + (days * 24 * 60 * 60);
+    playerInfo.IsLegend = true;
+    
+    playerInfoStore:Set(playerInfo);
+end
+
+function PlayerService.MakePermenantLegend(player)
+    local playerInfoStore = dataStore2(playerInfoData, player);
+    local playerInfo = playerInfoStore:Get();
+
+    playerInfo.IsLegend = true;
+    playerInfo.LegendExpiration = PERMENANT_LEGEND;
+
+    playerInfoStore:Set(playerInfo);
 end
 
 return PlayerService;
