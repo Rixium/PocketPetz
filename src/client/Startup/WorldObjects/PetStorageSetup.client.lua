d4c75a6de7440a332826dd4aa8fd8040c0e82611
Daniel Pottenger <gamerpotty@gmail.com>
2021-03-23 22:46:17 +0000

    fix pet storage and improve its ui

--- src/client/Startup/WorldObjects/PetStorageSetup.client.lua
+++ src/client/Startup/WorldObjects/PetStorageSetup.client.lua
@@ -14,17 +14,35 @@ local petStorages = collectionService:GetTagged("PetStorage");
 local currentItems = {};
 local currentStored = {};
 
+local SIZE = Vector2.new(0.21, 0.25);
+local PADDING = Vector2.new(0.03, 0.03);
+
 local storageGUI = uiManager.GetUi("Storage GUI");
+local mainGUI = uiManager.GetUi("Main GUI");
 local debounce = false;
 
 local storeCallback;
 local takeCallback;
 local refresh;
 
+local function ResetScroll(scrollingFrame)
+    local uiGridLayout = scrollingFrame.UIGridLayout;
+
+    local NewSize = SIZE * scrollingFrame.AbsoluteSize;
+    uiGridLayout.CellSize = UDim2.new(0, NewSize.X, 0, NewSize.Y);
+
+    local NewPadding = PADDING * scrollingFrame.AbsoluteSize
+    uiGridLayout.CellPadding = UDim2.new(0, NewPadding.X, 0, NewPadding .Y)
+    
+    scrollingFrame.CanvasSize = UDim2.new(0, uiGridLayout.AbsoluteContentSize.X, 0, uiGridLayout.AbsoluteContentSize.Y);
+end
+    
 local function SelectItem(selectedItem)
     local itemData = selectedItem.ItemData;
 
     local selectedFrame = storageGUI.ImageLabel.Frame.SelectedFrame;
+    selectedFrame.ImageFrame.Visible = true;
+    selectedFrame.StatFrame.Visible = true;
     
     local itemImage = selectedFrame.ImageFrame.ImageLabel;
     
@@ -39,17 +57,29 @@ local function SelectItem(selectedItem)
     itemImage.ItemImage.Image = "rbxthumb://type=Asset&id=" .. itemData.ModelId .. "&w=420&h=420";
 
     local statFrame = selectedFrame.StatFrame.ImageLabel;
-    statFrame.LevelFrame.TextLabel.Text = selectedItem.PlayerItem.Data.CurrentLevel;
+    statFrame.LevelFrame.TextLabel.Text = "Lvl. " .. selectedItem.PlayerItem.Data.CurrentLevel;
     statFrame.AttackFrame.TextLabel.Text = itemData.BaseAttack;
     statFrame.DefenceFrame.TextLabel.Text = itemData.BaseDefence;
 
-    local storeButton = storageGUI.ImageLabel.Frame.CurrentFrame.ContextButtonFrame.ContextButtonBack.ContextButton;
-    local takeButton = storageGUI.ImageLabel.Frame.StoredFrame.ContextButtonFrame.ContextButtonBack.ContextButton;
+    local storeButtonBack = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.StoreButton;
+    local storeButton = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.StoreButton.ContextButton;
+    local takeButtonBack = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.TakeButton;
+    local takeButton = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.TakeButton.ContextButton;
+    local leftArrow = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.LeftArrow;
+    local rightArrow = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.RightArrow;
+
+    storeButtonBack.Visible = false;
+    takeButtonBack.Visible = false;
+    leftArrow.Visible = false;
+    rightArrow.Visible = false;
 
     if(not selectedItem.PlayerItem.Data.InStorage) then
         if(storeCallback ~= nil) then
             storeCallback:Disconnect();
         end
+
+        storeButtonBack.Visible = true;
+        rightArrow.Visible = true;
 
         storeCallback = storeButton.MouseButton1Click:Connect(function()
             storeItem:InvokeServer(selectedItem);
@@ -60,6 +90,9 @@ local function SelectItem(selectedItem)
         if(takeCallback ~= nil) then
             takeCallback:Disconnect();
         end
+
+        takeButtonBack.Visible = true;
+        leftArrow.Visible = true;
 
         takeCallback = takeButton.MouseButton1Click:Connect(function()
             withdrawItem:InvokeServer(selectedItem);
@@ -97,6 +130,23 @@ local function AddItem(scrollingFrame, itemToAdd)
         tween.Completed:Wait();
         debounce = false;
     end)
+
+    item.ImageButton.MouseEnter:Connect(function() 
+        local hoverFrame = storageGUI:WaitForChild("HoverFrame");
+        local mouse = players.LocalPlayer:GetMouse();
+        hoverFrame.Position = UDim2.new(0, item.AbsolutePosition.X + item.AbsoluteSize.X + 5, 0, item.AbsolutePosition.Y + item.AbsoluteSize.Y + 5);
+        hoverFrame.C.TextLabel.Text = math.floor(itemToAdd.ItemData.BaseAttack);
+        hoverFrame.D.TextLabel.Text =  math.floor(itemToAdd.ItemData.BaseDefence);
+        hoverFrame.A.TextLabel.Text =  math.floor(itemToAdd.PlayerItem.Data.CurrentHealth) .. "/" .. math.floor(itemToAdd.ItemData.BaseHealth);
+        hoverFrame.B.TextLabel.Text =  "Lvl. " .. math.floor(itemToAdd.PlayerItem.Data.CurrentLevel);
+        hoverFrame.Visible = true;
+    end)
+
+    item.ImageButton.MouseLeave:Connect(function() 
+        local hoverFrame = storageGUI:WaitForChild("HoverFrame");
+        hoverFrame.Visible = false;
+    end)
+
     
     item.ThumbBack1.Image = "rbxthumb://type=Asset&id=" .. itemToAdd.ItemData.ModelId .. "&w=420&h=420";
     item.ThumbBack2.Image = "rbxthumb://type=Asset&id=" .. itemToAdd.ItemData.ModelId .. "&w=420&h=420";
@@ -125,13 +175,12 @@ local function setupStored(items)
         table.remove(currentStored, index);
     end
     
-    spawn(function ()
-        for _, item in pairs(items) do
-            if(item.ItemData.ItemType == "Pet" and item.PlayerItem.Data.InStorage) then
-                AddItem(scrollingFrame, item);
-            end
+    for _, item in pairs(items) do
+        if(item.ItemData.ItemType == "Pet" and item.PlayerItem.Data.InStorage) then
+            AddItem(scrollingFrame, item);
+            ResetScroll(scrollingFrame);
         end
-    end);
+    end
 end
 
 local function setupBackpack(items)
@@ -147,25 +196,44 @@ local function setupBackpack(items)
         table.remove(currentItems, index);
     end
     
-    spawn(function ()
-        for _, item in pairs(items) do
-            if(item.ItemData.ItemType == "Pet" and item.PlayerItem.Data.InStorage == false) then
-                AddItem(scrollingFrame, item);
-            end
+    for _, item in pairs(items) do
+        if(item.ItemData.ItemType == "Pet" and item.PlayerItem.Data.InStorage == false) then
+            AddItem(scrollingFrame, item);
+            ResetScroll(scrollingFrame);
         end
-    end);
+    end
 end
 
 local function show()
+    local selectedFrame = storageGUI.ImageLabel.Frame.SelectedFrame;
+    selectedFrame.ImageFrame.Visible = false;
+    selectedFrame.StatFrame.Visible = false;
+
+    local storeButtonBack = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.StoreButton;
+    local storeButton = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.StoreButton.ContextButton;
+    local takeButtonBack = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.TakeButton;
+    local takeButton = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.TakeButton.ContextButton;
+    local leftArrow = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.LeftArrow;
+    local rightArrow = storageGUI.ImageLabel.Frame.SelectedFrame.ContextButtonFrame.RightArrow;
+
+    storeButtonBack.Visible = false;
+    takeButtonBack.Visible = false;
+    leftArrow.Visible = false;
+    rightArrow.Visible = false;
+
     storageGUI.Enabled = true;
+    selectedItem = nil;
+    
 
     local items = getItemsRequest:InvokeServer();
 
-    setupBackpack(items);
-    setupStored(items);
+    spawn(function()    
+        setupBackpack(items);
+        setupStored(items);
+    end);
 
-    local tweenInfo = TweenInfo.new(0.7, Enum.EasingStyle.Elastic, Enum.EasingDirection.Out)
-    local tween = tweenService:Create(storageGUI.ImageLabel, tweenInfo, {Size=UDim2.new(0.8, 0, 0.7, 0)});
+    local tweenInfo = TweenInfo.new(0.7, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+    local tween = tweenService:Create(storageGUI.ImageLabel, tweenInfo, {Position=UDim2.new(0.5, 0, 0.5, 0)});
     tween:Play()
 end
 
